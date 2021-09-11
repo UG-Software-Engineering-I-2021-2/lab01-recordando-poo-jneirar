9fd6a6adead6efbe72dc01158289f21812efcb91
jneirar <jorge.neira@utec.edu.pe>
2021-09-11 09:50:30 -0500

    100%

--- src/main/java/cs/ing/software/lab1/Main.java
+++ src/main/java/cs/ing/software/lab1/Main.java
@@ -4,7 +4,8 @@ public class Main {
     static double fuelConsumoVehiculo = 0.9;
     static double fuelConsumoCamion = 1.6;
     static double tanqueCapacidad = 100;
+
     public static void main(String[] args) {
-        //Main function not needed
+        //Not needed
     }
 }
